1263c8710c98771a35cd47a7d813105e49b2d921
TmaneChouaib <chouaib.tmane@hotmail.com>
2023-04-19 23:59:36 +0000

    Implementation du CommandLineRunner

--- patients-mvc/src/main/java/ma/emsi/patientsmvc/PatientsMvcApplication.java
+++ patients-mvc/src/main/java/ma/emsi/patientsmvc/PatientsMvcApplication.java
@@ -1,7 +1,13 @@
 package ma.emsi.patientsmvc;
 
+import ma.emsi.patientsmvc.entities.Patient;
+import ma.emsi.patientsmvc.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class PatientsMvcApplication {
@@ -9,5 +15,18 @@ public class PatientsMvcApplication {
     public static void main(String[] args) {
         SpringApplication.run(PatientsMvcApplication.class, args);
     }
+    @Bean
+CommandLineRunner commandLineRunner(PatientRepository patientRepository){
+        return args -> {
+            patientRepository.save(new Patient(null,"Hassan",new Date(),false,12));
+            patientRepository.save(new Patient(null,"Ahmad",new Date(),false,14));
+            patientRepository.save(new Patient(null,"Saad",new Date(),true,8));
+            patientRepository.save(new Patient(null,"Khalid",new Date(),true,17));
 
+            patientRepository.findAll().forEach(p->{
+                System.out.println(p.getNom());
+            });
+
+        };
+}
 }
