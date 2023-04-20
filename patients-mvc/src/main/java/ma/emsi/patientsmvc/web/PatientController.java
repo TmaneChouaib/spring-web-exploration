6d75d104545b13f40df0d3fd9e3ecb1d0246278c
TmaneChouaib <chouaib.tmane@hotmail.com>
2023-04-20 13:33:07 +0000

    integration de la pagination

--- patients-mvc/src/main/java/ma/emsi/patientsmvc/web/PatientController.java
+++ patients-mvc/src/main/java/ma/emsi/patientsmvc/web/PatientController.java
@@ -3,9 +3,12 @@ package ma.emsi.patientsmvc.web;
 import lombok.AllArgsConstructor;
 import ma.emsi.patientsmvc.entities.Patient;
 import ma.emsi.patientsmvc.repositories.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,9 +18,11 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping(path="/index")
-    public String patients(Model model){
-        List<Patient> patients=patientRepository.findAll();
-        model.addAttribute("listPatients",patients);
+    public String patients(Model model ,
+                           @RequestParam(name="page",defaultValue = "0") int page ,
+                           @RequestParam(name="size",defaultValue = "5")  int size){
+        Page<Patient> pagePatients=patientRepository.findAll(PageRequest.of(page,size));
+        model.addAttribute("listPatients",pagePatients.getContent());
         //retourne une vue appele patients.html
         return "patients";
     }
